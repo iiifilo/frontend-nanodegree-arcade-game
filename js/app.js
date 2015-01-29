5086cbaa61800e4e7a5db7dfdf3731445b0658f5
iiifilo <mrochas@gmail.com>
2015-01-29 16:12:04 -0500

    Add all enemy objects in an array called allEnemies Add  var player = new Player

--- js/app.js
+++ js/app.js
@@ -29,7 +29,11 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+for (var i=0; i<gameState.enemyCount; i++) {
+    allEnemies.push(new Enemy());
+}
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
