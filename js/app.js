e8b0767e0a14066923b5bc298bef81bea858f771
iiifilo <mrochas@gmail.com>
2015-01-29 20:07:42 -0500

    Add Player update and Player Render(Now i can see something)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,7 +7,10 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
+
+//Declare Number of enemies
+var enemyCount = 6;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -19,7 +23,23 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
+
+//Player Objects
+var Player = function() {
+this.sprite = 'images/char-boy.png';
+};
+
+//Player Update
+Player.prototype.update = function(dt) {
+
+};
+
+//Player Render
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -29,11 +49,12 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-for (var i=0; i<gameState.enemyCount; i++) {
-    allEnemies.push(new Enemy());
-}
 var player = new Player();
+var allEnemies = [];
+var i = 0;
+for (var i=0; i<enemyCount; i++) {
+    allEnemies.push(new Enemy());
+};
 
 
 // This listens for key presses and sends the keys to your
